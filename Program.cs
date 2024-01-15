00bc9ad33473a6eb7a92dd032c814ee3ee496282
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-01-15 20:44:28 +0200

    added swagger

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => { c.EnableAnnotations(); });
 
 var connString = builder.Configuration.GetConnectionString("DefaultConnection");
 var serverVersion = new MySqlServerVersion(new Version(8, 2, 0));
@@ -20,7 +22,16 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseCors();
+
+
 
 app.MapControllers();
 app.Run();
